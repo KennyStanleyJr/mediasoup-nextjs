cb23a65d8fa6aecd07164b7754d5facb7232b652
Kenny Stanley <kpstanleyjr@gmail.com>
2021-07-06 14:36:54 -0500

    removed files

--- src/lib/socket.io-promise.ts
+++
@@ -1,9 +0,0 @@
-// Adds support for Promise to socket.io-client
-import { ISocket } from "./types"
-export default function promise(socket: ISocket) {
-    return function request(type: string, data = {}) {
-        return new Promise(resolve => {
-            socket.emit(type, data, resolve)
-        })
-    }
-}
